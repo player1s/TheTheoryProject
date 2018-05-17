ed1924d75dafda2d7877baa328bb290886c892d1
Sámán <31960469+player1s@users.noreply.github.com>
2018-05-17 15:14:58 +0200

    Project list adding exceptions
    Project list adding exceptions

--- SEP2/src/Model/ProjectList.java
+++ SEP2/src/Model/ProjectList.java
@@ -9,8 +9,19 @@ public class ProjectList {
 		this.list = new ArrayList<>();
 	}
 	
-	public void addProjet(Project proj) {
-		list.add(proj);
+	public void addProjet(Project proj) throws Exception
+	{
+		 for (int i = 0; i < list.size(); i++) {
+				if (list.get(i).getProjectID() == proj.getProjectID()) {
+					throw new Exception("Employee is already in");
+				}
+			}
+		if (proj.getProjectID() == 0 || proj.getDeadLine().equals("") || proj.getNameOfContractor().equals("") || proj.getWinningProposal().equals("")) {
+			throw new Exception("Employee can't be added");
+		}
+		else list.add(proj);
+		
+		
 	}
 	
 	public int getNumberOfProject() {
